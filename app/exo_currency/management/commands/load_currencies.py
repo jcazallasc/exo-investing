8c743fd0fdf80ca9e9737ec00e1c2f41b7b53409
Javier Cazallas <cazallas@MacBook-Pro-de-Javier.local>
2020-06-23 10:02:31 +0200

    Commands to populate DB

---
+++ app/exo_currency/management/commands/load_currencies.py
@@ -0,0 +1,39 @@
+import csv
+import os
+
+from django.core.management.base import BaseCommand, CommandError
+
+from exo_currency.models import Currency
+
+
+class Command(BaseCommand):
+    help = """Load currencies from CSV file. The CSV File must to be inside commands folder."""
+
+    def add_arguments(self, parser):
+        parser.add_argument('csv_filename', help='CSV filename', type=str)
+
+    def get_full_path(self, csv_file):
+        return os.path.join(os.path.dirname(__file__), csv_file)
+
+    def handle(self, *args, **options):
+        csv_filename = options.get('csv_filename')
+        csv_file = self.get_full_path(csv_filename)
+
+        try:
+            with open(csv_file, mode='r') as csv_file:
+                csv_reader = csv.DictReader(
+                    csv_file, delimiter=';',
+                    quoting=csv.QUOTE_NONE,
+                )
+
+                for row in csv_reader:
+                    Currency.objects.update_or_create(
+                        code=row['code'],
+                        defaults={
+                            'name': row['name'],
+                            'symbol': row['symbol'],
+                        },
+                    )
+                print('Import currencies done!')
+        except FileNotFoundError as error:
+            raise CommandError(error)
