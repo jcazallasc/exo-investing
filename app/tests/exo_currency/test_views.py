d48fcaa04e1f858c1ea6e32ca8bd042d5f1dd7d5
Javier Cazallas <cazallas@MacBook-Pro-de-Javier.local>
2020-06-24 09:14:27 +0200

    Patching future calls

--- app/tests/exo_currency/test_views.py
+++ app/tests/exo_currency/test_views.py
@@ -1,4 +1,6 @@
+import json
 from datetime import date
+from unittest.mock import MagicMock, patch
 
 from django.contrib.auth import get_user_model
 from django.core.management import call_command
@@ -30,8 +32,25 @@ class ExoCurrencyV1ApiTests(TestCase):
         if not currency_exchange_rates:
             call_command('load_exchange_rates', 'data.csv')
 
-    def test_exchange_rate_evolution_view_with_no_params(self):
+    @patch('requests.get')
+    def test_exchange_rate_evolution_view_with_no_params(self, mock_get):
         """Test the exchange rate evolution view works with no params"""
+
+        mock_return_value = MagicMock()
+        mock_return_value.status_code = 200
+        mock_return_value.text = json.dumps({
+            'success': True,
+            'imestamp': 1363478399,
+            'historical': True,
+            'base': 'EUR',
+            'date': '2020-06-23',
+            'rates': {
+                'USD': 1.307716,
+                'CHF': 1.307716,
+                'GBP': 1.307716,
+            }
+        })
+        mock_get.return_value = mock_return_value
 
         res = self.client.get(reverse(EXCHANGE_RATE_EVOLUTION))
 
